f02669cd5e02579626e386805b8772bf0995a5f6
A_SwitchCase <adamsjacques546@gmail.com>
2024-01-28 15:34:02 -0500

    Add materal UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import MyButton from './Components/MyButton'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <h1>Hello World</h1>
+    <MyButton></MyButton>
   </React.StrictMode>,
 )
--- src/renderer.ts
+++ src/renderer.ts
@@ -28,4 +28,9 @@
 
 import './index.css';
 import './App';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
